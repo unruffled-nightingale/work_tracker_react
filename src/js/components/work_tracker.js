7e38b3cfe10adc05005321d2138c301e42d0eeb8
unruffled-nightingale <unruffled.nightingale@gmail.com>
2018-07-28 10:40:27 +0100

    just blocking some code

--- src/js/components/work_tracker.js
+++ src/js/components/work_tracker.js
@@ -8,97 +8,91 @@ export default class Main extends Component {
      this.api_url = 'http://localhost:5000'
      this.user = {'user_name': 'robert manteghi', 'user_id': 1};
      this.addTask = this.addTask.bind(this);
+     this.deleteTask = this.deleteTask.bind(this);
      this.changeTaskInput = this.changeTaskInput.bind(this);
+     this.changeTask = this.changeTask.bind(this);
+
+     // for testing
+     this.task_count = 1
 
      this.state = {
-       tasks: ["option2", "break", "option1", 'hello'],
-       inputTask: "",
-       currentTask: "hello"
+         tasks: [{"task_id": 0, "task": "break"}],
+         inputTask: "",
+         currentTask: "break",
+         user_id: 0
      }
 
    }
 
-   getUserId(user_name){
-      var user_id = ""
-      console.log('posting '+this.state.inputTask+' to /get_user');
-      return user_id
-   }
-
-   postTask( task ) {
+   postTask (task) {
        var task_id = ""
-       console.log('posting '+this.state.inputTask+' to /addTask');
+       console.log('posting to /post_task: ' + task);
+       this.task_count++
+       task_id = this.task_count
        return task_id
    }
 
-   logTask ( user_id, task_id, task ) {
-        console.log('posting to /logTask');
+   postUser (user) {
+       var user_id = ""
+       console.log('posting to /add_user: ' + task);
+       user_id = 1
+       return user_id
    }
 
-   changeTaskInput(e){
-      /* Changes the state of inputTask based on the status of the input form */
-      this.setState({inputTask : e.target.value})
+   logTask (task_id, user_id) {
+       console.log('posting to /log_task: ' + task_id + ' ' + user_id)
    }
 
    addTask () {
-     /* Adds a task to the list of tasks and posts the task to the api */
-     if (!this.state.tasks.includes(this.state.inputTask) && this.state.inputTask!=="") {
-         var task_id = ""
-         task_id = postTask(this.state.inputTask)
-         this.state.tasks.push(this.state.inputTask)
+     if (!this.state.tasks.map(e => e.task).includes(this.state.inputTask) && this.state.inputTask!=="") {
+         var task_id =  this.postTask(this.state.inputTask)
+         var task = ({'task_id': task_id, 'task': this.state.inputTask})
+         this.setState(prevState => ({ tasks: [...prevState.tasks, task]}));
      }
    }
 
-   handleOptionChange = (e) => {
-
-    this.setState({
-      currentTask: e.target.value
-    });
-    if (this.state.currentTask === e.target.value){
-        console.log('same')
-    }
+   deleteTask(task_id, task) {
+      console.log(e.target.task_id)
+      console.log(task)
+      // find position
+      var array = [...this.state.tasks]
+      var index = array.indexOf({'task_id': task_id, 'task': task})
+      array.splice(index, 1)
+      this.setState({tasks: array})
    }
 
-   renderRadioButton = (value) => {
-       return (
-           <div className="radio">
+   changeTask = (e) => {
+       this.setState({currentTask: e.target.value})
+       this.logTask(e.target.id, this.state.user_id)
+   }
+
+   changeTaskInput(e){
+      this.setState({inputTask : e.target.value})
+   }
+
+   renderRadioButton = (id, value) => {
+      return (
+          <div className="radio">
               <label>
-                  <input type="radio" value={value} checked={this.isChecked} onChange={this.handleOptionChange} />
+                  <input type="radio" value={value} id={id} checked={this.state.currentTask === value}
+                      onChange={this.changeTask} />
                   {value}
-            </label>
-           </div>
-       )
+              </label>
+              <button type='button' onClick={this.deleteTask}><i class="material-icons">DELETE</i></button>
+          </div>
+      )
    }
-
 
    render() {
       return (
-         <div>
-             <h1>WorkTracker</h1>
-             <input type="text" onChange={this.changeTaskInput}/>
-             <button onClick={this.addTask}><i class="material-icons">ADD</i></button>
-             <form>
-              {this.renderRadioButton('hello2')}
-              <div className="radio">
-                <label>
-                  <input type="radio" value="hello" checked={this.state.currentTask === 'hello'} onChange={this.handleOptionChange} />
-                  hello
-                </label>
-              </div>
-              <div className="radio">
-                <label>
-                  <input type="radio" value="option2" checked={this.state.currentTask === 'option2'} onChange={this.handleOptionChange}/>
-                  Option 2
-                </label>
-              </div>
-              <div className="radio">
-                <label>
-                  <input type="radio" value="option3" checked={this.state.currentTask === 'option3'} onChange={this.handleOptionChange}/>
-                  Option 3
-                </label>
-              </div>
-            </form>
-         </div>
-
+          <div>
+              <h1>WorkTracker</h1>
+              <input type="text" onChange={this.changeTaskInput}/>
+              <button onClick={this.addTask}><i class="material-icons">ADD</i></button>
+              <form>
+                { this.state.tasks.map(e => this.renderRadioButton(e.task_id, e.task) )}
+              </form>
+          </div>
       )
-    }
+   }
 };
